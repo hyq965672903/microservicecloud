e29d177ae8ed3a24b95a0b104cb1aa30ccd2b538
hyq965672903 <965672903@qq.com>
2018-10-14 19:01:25 +0800

    Feign
    Feign面向接口编程
    Feign通过接口的方法调用Rest服务（之前是Ribbon+RestTemplate）

---
+++ microservicecloud-consumer-dept-feign/src/main/java/com/hyq/springcloud/cfgbean/ConfigBean.java
@@ -0,0 +1,33 @@
+package com.hyq.springcloud.cfgbean;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RetryRule;
+
+@Configuration
+public class ConfigBean {
+	@Bean
+	@LoadBalanced // Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端 负载均衡的工具。
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	/**
+	 * 负载均衡算法
+	 * @return
+	 */
+	@Bean
+	public IRule myRule() {
+		return new  RetryRule();
+	}
+}
+// @Bean
+// public UserServcie getUserServcie()
+// {
+// return new UserServcieImpl();
+// }
+// applicationContext.xml == ConfigBean(@Configuration)
+// <bean id="userServcie" class="com.atguigu.tmall.UserServiceImpl">
